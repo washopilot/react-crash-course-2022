84403b8cc6fc06044978b0b83a741fef0864e976
Fernando chicaiza <washopilot@yahoo.com>
2022-05-05 18:23:22 -0500

    seguimos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    contador: 0,
-  };
+interface Estado {
+  contador: number;
+}
+
+class App extends Component<{}, Estado> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      contador: 10,
+    };
+  }
   render() {
     return (
       <div className="App">
-        <button onClick={() => this.setState({ contador: this.state.contador - 1 })}>-</button>
+        <button onClick={() => console.log('click')}>-</button>
         <h1>{this.state.contador}</h1>
-        <button onClick={() => this.setState({ contador: this.state.contador + 1 })}>+</button>
       </div>
     );
   }
